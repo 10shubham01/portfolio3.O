8144a1d818b83a2c8193df20110de718add5cd43
Shubham Gupta <shubhamedu.01@gmail.com>
2025-10-14 19:12:28 +0530

    fix

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,92 +1,76 @@
-"use client";
-import React, { useState } from "react";
-import { motion, LayoutGroup } from "framer-motion";
+import BentoGrid from "@/components/BentoGrid";
+import React from "react";
+export const projects = [
+  {
+    title: "Stripe",
+    id: 1,
+    description:
+      "A technology company that builds economic infrastructure for the internet.",
+    link: "https://stripe.com",
+  },
+  {
+    id: 2,
 
-const itemsData = [
-  { id: 1, title: "Item 1", description: "Description for item 1" },
-  { id: 2, title: "Item 2", description: "Description for item 2" },
-  { id: 3, title: "Item 3", description: "Description for item 3" },
-  { id: 4, title: "Item 4", description: "Description for item 4" },
-  { id: 5, title: "Item 5", description: "Description for item 5" },
+    title: "Netflix",
+    description:
+      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+    link: "https://netflix.com",
+  },
+  {
+    id: 3,
+    title: "Google",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "https://google.com",
+  },
+  {
+    id: 4,
+
+    title: "Meta",
+    description:
+      "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+    link: "https://meta.com",
+  },
+  {
+    id: 5,
+    title: "Amazon",
+    description:
+      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    link: "https://amazon.com",
+  },
+  {
+    id: 6,
+    title: "Amazon",
+    description:
+      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    link: "https://amazon.com",
+  },
+  {
+    id: 7,
+    title: "Amazon",
+    description:
+      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    link: "https://amazon.com",
+  },
+  {
+    id: 8,
+    title: "Amazon",
+    description:
+      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    link: "https://amazon.com",
+  },
+  {
+    id: 9,
+    title: "Amazon",
+    description:
+      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    link: "https://amazon.com",
+  },
 ];
-
-const Page = () => {
-  const [items, setItems] = useState(itemsData);
-
-  const handleClick = (id: number) => {
-    const selectedItem = items.find((item) => item.id === id);
-    if (!selectedItem) return;
-    setItems([
-      selectedItem,
-      ...items.filter((item) => item.id !== id).sort((a, b) => a.id - b.id),
-    ]);
-  };
-
+export default function page() {
   return (
-    <div className="min-h-screen w-full flex justify-center items-center bg-gray-50 p-4 sm:p-6">
-      <div className="rounded-md w-full max-w-6xl p-2 relative overflow-hidden">
-        {/* Decorative Borders */}
-        <div className="absolute top-0 left-1/2 w-1/2 h-px bg-gradient-to-r from-transparent to-gray-200"></div>
-        <div className="absolute bottom-0 right-0 w-1/2 h-px bg-gradient-to-l from-gray-200 to-transparent"></div>
-        <div className="absolute right-0 top-0 h-full w-px bg-gradient-to-b from-gray-200 to-gray-200"></div>
-
-        <LayoutGroup>
-          <div
-            className="
-              grid gap-2
-              grid-cols-1
-              sm:grid-cols-4
-              auto-rows-[minmax(120px,1fr)]
-              transition-all
-            "
-          >
-            {items.map((item, index) => (
-              <motion.div
-                key={item.id}
-                layout
-                transition={{
-                  layout: { duration: 0.9, type: "spring", bounce: 0.25 },
-                }}
-                onClick={() => handleClick(item.id)}
-                className={`
-                  border bg-white rounded-xl flex flex-col justify-center items-center
-                  cursor-pointer overflow-hidden transition-colors
-                  hover:bg-black hover:text-white
-                  ${
-                    index === 0
-                      ? "lg:col-span-2 lg:row-span-2 sm:col-span-2 sm:row-span-1 hover:bg-white hover:!text-black"
-                      : "aspect-square"
-                  }
-                `}
-              >
-                <motion.h2
-                  layout="position"
-                  className={`font-semibold ${
-                    index === 0 ? "text-2xl mb-2" : "text-lg"
-                  }`}
-                >
-                  {item.title}
-                </motion.h2>
-
-                {index === 0 && (
-                  <motion.p
-                    layout="position"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 0.3 }}
-                    className="text-gray-500 text-center max-w-sm text-sm sm:text-base"
-                  >
-                    {item.description}
-                  </motion.p>
-                )}
-              </motion.div>
-            ))}
-          </div>
-        </LayoutGroup>
-      </div>
+    <div className=" w-screen flex justify-center items-center">
+      <BentoGrid className="max-w-6xl" items={projects}></BentoGrid>
     </div>
   );
-};
-
-export default Page;
+}
---
+++ components/BentoGrid.tsx
@@ -0,0 +1,149 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
+
+import { useState } from "react";
+
+const BentoGrid = ({
+  items,
+  className,
+}: {
+  items: {
+    title: string;
+    id: number;
+    description: string;
+    link: string;
+  }[];
+  className?: string;
+}) => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [localitems, setItems] = useState(items);
+
+  const handleClick = (id: number) => {
+    const selectedItem = localitems.find((item) => item.id === id);
+    if (!selectedItem) return;
+    setItems([
+      selectedItem,
+      ...localitems
+        .filter((item) => item.id !== id)
+        .sort((a, b) => a.id - b.id),
+    ]);
+  };
+
+  return (
+    <div
+      className={cn(
+        "relative p-4 rounded-3xl overflow-hidden border",
+        className
+      )}
+    >
+      <LayoutGroup>
+        <div
+          className={cn(
+            "grid gap-2 grid-cols-1 sm:grid-cols-4 auto-rows-[minmax(120px,1fr)] transition-all relative"
+          )}
+        >
+          {localitems.map((item, idx) => (
+            <motion.div
+              key={item?.id}
+              onMouseEnter={() => setHoveredIndex(idx)}
+              onMouseLeave={() => setHoveredIndex(null)}
+              layout
+              transition={{
+                layout: { duration: 0.7, type: "spring", bounce: 0.25 },
+              }}
+              onClick={() => handleClick(item.id)}
+              className={`
+                    relative group  block p-2
+                  cursor-pointer  
+                  ${
+                    idx === 0
+                      ? "lg:col-span-2 lg:row-span-2 sm:col-span-2 sm:row-span-1"
+                      : "aspect-square"
+                  }
+                `}
+            >
+              <AnimatePresence>
+                {hoveredIndex === idx && idx !== 0 && (
+                  <motion.span
+                    className="absolute inset-0 h-full w-full border block  rounded-3xl"
+                    layoutId="hoverBackground"
+                    initial={{ opacity: 1 }}
+                    animate={{
+                      opacity: 1,
+                      transition: { duration: 0.15 },
+                    }}
+                    exit={{
+                      opacity: 1,
+                      transition: { duration: 0.15, delay: 0.2 },
+                    }}
+                  />
+                )}
+              </AnimatePresence>
+              <Card>
+                <CardTitle>{item.title}</CardTitle>
+                {idx === 0 && (
+                  <CardDescription>{item.description}</CardDescription>
+                )}
+              </Card>
+            </motion.div>
+          ))}
+        </div>
+      </LayoutGroup>
+    </div>
+  );
+};
+
+export const Card = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        className
+      )}
+    >
+      <div className="relative z-50">
+        <div className="p-4">{children}</div>
+      </div>
+    </div>
+  );
+};
+export const CardTitle = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+      {children}
+    </h4>
+  );
+};
+export const CardDescription = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <p
+      className={cn(
+        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
+};
+
+export default BentoGrid;
