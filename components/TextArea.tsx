ed1c1a884334d09989f23ab46060411f65e9cf8e
Shubham Gupta <shubhamedu.01@gmail.com>
2025-11-19 15:03:06 +0530

    log

--- components/TextArea.tsx
+++ components/TextArea.tsx
@@ -11,6 +11,9 @@ const poppins = Poppins({
 export default function AnimatedTextarea() {
   const [height, setHeight] = useState("auto");
   console.log(height);
+  console.log(height);
+  console.log(height);
+
 
   return (
     <div className="flex justify-end flex-col">
